ed659fcecea3b429184f02865c12f0b4fa95b117
Soumya Ranjan Kalia <a2soumya.ranjan@gmail.com>
2023-01-07 13:42:59 +0530

    hero content

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import { NavBar } from './components/NavBar'
-
+import { HeroContent } from './components/HeroContent'
 
 function App() {
 
   return (
     <div className="App">
       <NavBar/>
-    
+      <HeroContent/>
+    </div>
   )
 }
 
---
+++ src/components/HeroContent.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const HeroContent = () => {
+  return (
+    <div className='heroDiv'>
+        <div className='heroDiv-one'>
+            <h1>Driven by Outcomes, <br/> Fuelled by Ambitions.</h1>
+        </div>
+        <div className='heroDiv-two'>
+            <p className='heroDiv-two-pOne'>Launch your dream career </p>
+            <p className='heroDiv-two-pTwo'>at 0 upfront fee.</p>
+            <img src="https://www.masaischool.com/img/homepage/scribble.png" alt="" />
+        </div>
+        <div className='heroDiv-three'>
+            <button>APPLY NOW FOR FREE</button>
+        </div>
+    </div>
+  )
+}
